67be29d4e6c8b18395e486e85701f7fcfee0b1a3
Alpha-Thulz <tj.tyaphile@gmail.com>
2026-01-13 20:52:13 +0200

    Build navigational bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
-import './App.css'
+import {AppNav} from "./components/AppNav.tsx";
 
 function App() {
   return (
-      <div className="p-3 mb-2 bg-dark text-white" data-bs-theme="light">
-          <h1>The site is under a huge revamp and relevance upgrade :)</h1>
-      </div>
+      <>
+          <AppNav/>
+          <div className="p-3 mb-2 bg-dark text-white" data-bs-theme="light">
+              <h1>First WEDELA website</h1>
+          </div>
+      </>
   )
 }
 
